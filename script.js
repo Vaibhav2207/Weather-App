2f8e2821d742db3404df7206633932f69f77685d
Vaibhav2207 <128115456+Vaibhav2207@users.noreply.github.com>
2023-08-07 20:03:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '53a62abdbdmshd86bf5a8f380209p195b4ajsnd2980d0ef348',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
